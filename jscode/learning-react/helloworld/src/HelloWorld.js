41b3a51c26f45d4a6bc1266f7faaa5a1b35963e5
tamlt2704 <tamlt2704@gmail.com>
2020-04-24 01:48:35 +0000

    setting up environment easily

---
+++ jscode/learning-react/helloworld/src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div className='helloContainer'>
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
+
+export default HelloWorld;
