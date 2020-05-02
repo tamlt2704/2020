9ade2a600d61dc8889909cb2b4e48f6612e445e5
tamlt2704 <tamlt2704@gmail.com>
2020-05-02 13:02:41 +0000

    02. bootstrap icon setup

--- full-stack-react/02_time_logging_app/src/App.js
+++ full-stack-react/02_time_logging_app/src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faCoffee, faSortUp} from '@fortawesome/free-solid-svg-icons'
  
-library.add(fab, faCheckSquare, faCoffee)
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class TimerDashboard extends Component {
     render() {
         return (
@@ -19,23 +20,29 @@ class TimerDashboard extends Component {
     }
 }
 
+class EditableTimerList extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+class ToogleableTimerForm extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <TimerDashboard />
     </div>
   );
 }
--- full-stack-react/Readme.md
+++ full-stack-react/Readme.md
@@ -19,17 +19,5 @@ npm install react-bootstrap bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //bootstrap icon
-npm i --save @fortawesome/fontawesome-svg-core
-@fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome
-npm i --save @fortawesome/free-brands-svg-icons
+official documentation: https://github.com/FortAwesome/react-fontawesome
 
-App.js
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
- 
-library.add(fab, faCheckSquare, faCoffee)
-
-XYZ.js
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-<FontAwesomeIcon icon='coffee' />
