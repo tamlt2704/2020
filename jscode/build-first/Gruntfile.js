d1b89efb01b8c48bc6451fc94ec39822d98ad1d9
tamlt2704 <tamlt2704@gmail.com>
2020-05-03 06:05:34 +0000

    build-first custom grunt task

--- jscode/build-first/Gruntfile.js
+++ jscode/build-first/Gruntfile.js
@@ -1,10 +1,33 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        jshint: ['Gruntfile.js']
+        jshint: ['Gruntfile.js'],
+        concat: {
+            js: {
+                files: {
+                    'build/js/bundle.js': 'public/js/**/*.js'
+                }
+            }
+        },
+        timestamp: {
+            options: {
+                file: '.mytimestamp'
+            }
+        }
     });
     
     // plugins need to be loaded individually into Grunt
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
     grunt.registerTask('default', ['jshint']); 
+
+    grunt.registerTask('timestamp', function() {
+        var options = this.options({
+            file: '.timestamp'
+        });
+
+        var timestamp = +new Date();
+        var contents = timestamp.toString();
+
+        grunt.file.write(options.file, contents);
+    });
 };
--- jscode/build-first/Readme.md
+++ jscode/build-first/Readme.md
@@ -6,4 +6,29 @@ npm install --save-dev grunt
 
 // grunt js hint
 npm install --save-dev grunt-contrib-jshint
+npm install --save-dev grunt-contrib-less
+npm install --save-dev grunt-contrib-concat  // create bundle file
+npm install --save-dev grunt-contrib-uglify  // minify files
+npm install --save-dev grunt-spritesmith     // image sprites
+npm install --save-dev grunt-contrib-clean   // remove generated content
 
+
+// custom task
+grunt.registerTask('timestamp', function() {
+    var options = this.options({
+        file: '.timestamp'
+    });
+
+    var timestamp = +new Date();
+    var contents = timestamp.toString();
+
+    grunt.file.write(options.file, contents);
+});
+
+grunt.initConfig({
+    timestamp: {
+        options: {
+            file: '.mytimestamp'
+        }
+    }
+});
