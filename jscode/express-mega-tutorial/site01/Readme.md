d6b6539e9b3078ecb1e89824812e702a8beedb7f
tamlt2704 <tamlt2704@gmail.com>
2020-05-21 19:17:45 +0800

    simple express layout

---
+++ jscode/express-mega-tutorial/site01/Readme.md
@@ -0,0 +1,17 @@
+npm init -y
+npx gitignore node
+npm install express
+
+
+low level
+res.end         ->    res.send
+res.writeHead   ->    res.status
+
+
+app.use: express middleware
+
+order in which routes and middleware are added is significant
+
+Template
+    npm install express-handlebars
+    
---
+++ jscode/express-mega-tutorial/site01/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}));
+
+app.set('view engine', 'handlebars');
+
+const port = process.env.PORT || 5000;
+
+/*
+app.get('/', (req, res) => {
+    res.type('text/plan');
+    res.send('Hello From ExpressJs');
+});
+*/
+app.get('/', (req, res) => res.render('home'));
+app.get('/about', (req, res) => res.render('about'));
+
+app.use((req, res) => {
+    /*
+    res.type('text/plan');
+    res.status(404);
+    res.send('404 - Not Found');
+    */
+    res.status(404);
+    res.render('404'); 
+});
+
+app.use((err, req, res, next) => {
+    /*
+    console.log(err.message);
+    rest.type('text/plan');
+    res.status(500);
+    res.send('500 - Sever Error');
+    */
+    console.log(err.message);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(port, () => console.log(  
+    `Express started on http://localhost${port}` +
+    `press Ctrl-C to terminate.`
+));
