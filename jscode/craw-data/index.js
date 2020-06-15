0380508f9484b70b2d5398b483a3e49c2be2adcf
tamlt2704 <tamlt2704@gmail.com>
2020-06-15 14:47:33 +0800

    craw data

--- jscode/craw-data/index.js
+++ jscode/craw-data/index.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer')
 const moment = require('moment')
+const fs = require('fs')
 
 void(
     async () => {
@@ -14,19 +15,32 @@ void(
             await page.goto('https://portal.vietcombank.com.vn/Personal/TG/Pages/ty-gia.aspx')
             
             const input = await page.$('#txttungay')
-            await input.click({clickCount: 3})
-            await input.type('01/06/2020')
-            await page.keyboard.press(String.fromCharCode(13))
-            await page.keyboard.press('Enter')
-            await page.waitFor('#interest-rates')
-            await page.waitFor(5000)
-            // const element = await page.$('#interest-rates')
-            const element = await page.$('#ctl00_Content_ExrateView')
-            const text = await page.evaluate(element => element.innerHTML, element)
-            console.log( text )
-            await page.screenshot({
-                path: './page1.png'
-            })
+
+            var datetime = new Date('2020-01-01 00:00:00')
+            for (var i = 0; i < 10; i++) {
+                var newdate = moment(datetime, 'YYYY-MM-DD').add(i, 'days')
+                console.log( newdate.format('DD/MM/YYYY') )
+
+                await input.click({clickCount: 3})
+                // await input.type('01/06/2020')
+                await input.type( newdate.format('DD/MM/YYYY') )
+                await page.keyboard.press(String.fromCharCode(13))
+                await page.keyboard.press('Enter')
+                await page.waitFor('#interest-rates')
+                await page.waitFor(10000)
+                // const element = await page.$('#interest-rates')
+                const element = await page.$('#ctl00_Content_ExrateView')
+                if (element) {
+                    const text = await page.evaluate(element => element.innerHTML, element)
+                    var filename = newdate.format('YYYY-MM-DD')
+                    fs.writeFile(filename, text, (err) => {
+                        if (err) {
+                            console.log( 'error while saving data', err)
+                        }
+                        console.log( filename , 'saved')
+                    })
+                }
+            }
 
             await browser.close()
         } catch (err) {
@@ -35,8 +49,3 @@ void(
     }
 )()
 
-var datetime = new Date('2020-01-01 00:00:00')
-for (var i = 0; i < 10; i++) {
-    var newdate = moment(datetime, 'YYYY-MM-DD').add(i, 'days')
-    console.log( newdate.format('YYYY-MM-DD') )
-}
