11db4c5728b8aea2cdcc8a6c2c4236441bfc35f7
debian <debian@DESKTOP-Q0HSLBF.debian>
2020-08-17 05:48:44 -0700

    learn prolog now ch1

---
+++ learn-prolog-now/ch1/kb1.pl
@@ -0,0 +1,10 @@
+woman(mia).
+woman(jody).
+woman(yolanda).
+playsAirGuitar(jody).
+party.
+
+?- woman(mia).
+?- woman(jody).
+?- playsAirGuitar(jody).
+
---
+++ learn-prolog-now/ch1/kb3.pl
@@ -0,0 +1,9 @@
+happy(vincent).
+listens2music(butch).
+playsAirGuitar(vincent):-
+  listens2music(vincent),
+  happy(vincent).
+playsAirGuitar(butch):-
+  happy(butch).
+playsAirGuitar(butch):-
+  listens2music(butch).
